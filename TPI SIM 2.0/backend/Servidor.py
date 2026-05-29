b3fa76dce5d14fd9a1c5c9958ce2c399021659d4
gero04 <gveranovillo@gmail.com>
2026-05-29 11:45:38 -0300

    comentado el servidor

--- TPI SIM 2.0/backend/Servidor.py
+++ TPI SIM 2.0/backend/Servidor.py
@@ -1,9 +1,11 @@
+# aca modelamos el servidor de vacunacion 
+
 class ServidorVacunacion:
 
     def __init__(self, id_servidor):
 
         # =========================
-        # IDENTIFICACIÓN
+        # IDENTIFICACIÓN -> identificador único del servidor
         # =========================
 
         self.id = id_servidor
@@ -16,27 +18,29 @@ class ServidorVacunacion:
         # - Libre
         # - Ocupado
         # - Bloqueado
+        # Por defecto el servidor esta inicialmente libre
 
         self.estado = "Libre"
 
         # =========================
-        # PACIENTE ACTUAL
+        # PACIENTE ACTUAL: referencia a la instancia del objeto paciente que está atendiendo en ese momento
         # =========================
 
         self.paciente_actual = None
 
         # =========================
-        # EVENTOS FUTUROS
+        # Fin de vacunacion: referencia al momento en el que se termina de vacunar al paciente, es decir, se libera al servidor de la instancia del objeto que definimos arriba
         # =========================
 
         self.fin_vacunacion = None
 
         # =========================
-        # BLOQUEO
+        # BLOQUEO: lo usamos para calcular cuanto tiempo el servidor esta bloqueado cuando no hay espacio en la zona de observacion
         # =========================
 
         self.inicio_bloqueo = None
 
+    # Le asgina un paciente al servidor y lo pone como Ocupado
     def ocupar(self, paciente, hora_fin_vacunacion):
 
         self.estado = "Ocupado"
@@ -46,6 +50,7 @@ class ServidorVacunacion:
 
         self.fin_vacunacion = round(hora_fin_vacunacion, 3)
 
+    # Libera al servidor despues de que el pacien se transfiere a observacion reiniciando todos los atributos a valores por defecto
     def liberar(self):
 
         self.estado = "Libre"
@@ -56,6 +61,7 @@ class ServidorVacunacion:
 
         self.inicio_bloqueo = None
 
+    # Bloquea el servidor porque esta llena la zona de observacion 
     def bloquear(self, reloj_actual):
 
         self.estado = "Bloqueado"
@@ -65,6 +71,7 @@ class ServidorVacunacion:
         # La vacunación YA terminó
         self.fin_vacunacion = None
 
+    # Esto es para logging
     def __str__(self):
 
         return (
